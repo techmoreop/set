73820418bd77d627eeabc61ef10589e477799757
techmoreop <techmoreop@gmail.com>
2026-03-01 20:28:25 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array as arr
-a = arr.array('i', [1, 3, 5, 3, 7, 9, 3])
+s = {25,10,12,27,26,25,}
+a = {"meow", 83, 25.11, True}
+print(s)
 print(a)
-print(a.count(3))
-a.reverse()
-print(a)
+p = [1,2,3,4,3,2,4]
+print (p)
+p = set(p)
+p.pop()
+print(p)
